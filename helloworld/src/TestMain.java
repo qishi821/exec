bfe77d7664119324efd45c8d5663a716ff56043f
jujiantao <jujiantao@guazi.com>
2019-08-10 20:02:51 +0800

    kaifa2

--- helloworld/src/TestMain.java
+++ helloworld/src/TestMain.java
@@ -3,5 +3,6 @@ public class TestMain {
         System.out.println("zongbu");
 	System.out.println("fenzongbu");
 	System.out.println("fenfenzongbukaifa1");
+	System.out.println("fenfenzongbukaifa2");
     }
 }
