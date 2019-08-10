061742624b3a812ef8c250bdb6d5cfe8ab97d344
qishi821 <jjt_eagle@163.com>
2019-08-10 19:22:59 +0800

    Update TestMain.java

--- helloworld/src/TestMain.java
+++ helloworld/src/TestMain.java
@@ -1,10 +1,5 @@
 public class TestMain {
     public static void main(String[] args) {
-<<<<<<< HEAD
-        System.out.println("jjt");
-=======
-        System.out.println("hello world update");
-	System.out.println("add110");
->>>>>>> kaifa
+        System.out.println("zongbu");
     }
 }
