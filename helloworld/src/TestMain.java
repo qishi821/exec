4b70ca36a7d3fb07b6751592cbb294821471ad99
jujiantao <jujiantao@guazi.com>
2019-08-10 19:14:21 +0800

    update

--- helloworld/src/TestMain.java
+++ helloworld/src/TestMain.java
@@ -3,10 +3,9 @@ public class TestMain {
         System.out.println("hello world update");
         System.out.println("add1");
         System.out.println("add2");
-<<<<<<< HEAD
         System.out.println("add4");
-=======
         System.out.println("add3");
->>>>>>> b500379ad11798cbac28a0f6ae703666fa245b2d
+	System.out.println("add5");
+	System.out.println("add6");
     }
 }
