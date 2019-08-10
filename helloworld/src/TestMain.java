6931fc95791be782072fd30aee782148dc6ff8d0
qishi821 <jjt_eagle@163.com>
2019-08-10 19:43:13 +0800

    Update TestMain.java

--- helloworld/src/TestMain.java
+++ helloworld/src/TestMain.java
@@ -1,7 +1,5 @@
 public class TestMain {
     public static void main(String[] args) {
         System.out.println("zongbu");
-	System.out.println("fenzongbu");
-	ystem.out.println("fenfenzongbu");
     }
 }
