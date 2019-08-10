bc8930a4f6396ee6e547608ed10e87fe9a141ad1
jujiantao <jujiantao@guazi.com>
2019-08-10 19:56:48 +0800

    fenzongbukai1

--- helloworld/src/TestMain.java
+++ helloworld/src/TestMain.java
@@ -2,5 +2,6 @@ public class TestMain {
     public static void main(String[] args) {
         System.out.println("zongbu");
 	System.out.println("fenzongbu");
+	System.out.println("fenfenzongbukaifa1");
     }
 }
